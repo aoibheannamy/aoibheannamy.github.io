d661307a123e9a29589d1350699b869a66e29691
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-08 17:27:12 +0100

    u

--- javascript/getCharacterImage.js
+++ javascript/getCharacterImage.js
@@ -11,7 +11,7 @@ function setCharacterImage() {
 
     try {
         // use the JSON.stringify() method to convert the data into a string before attempting to parse it
-        var stringified = JSON.stringify(characters);
+        //var stringified = JSON.stringify(characters);
         // Now characterImage is the parsed result
         characterImage = JSON.parse(characters);
 

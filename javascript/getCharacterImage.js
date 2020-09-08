c95998df2326753eb32dc9d298188e9ef27435e6
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-08 18:57:44 +0100

    again

--- javascript/getCharacterImage.js
+++ javascript/getCharacterImage.js
@@ -24,7 +24,7 @@ function setCharacterImage() {
     alert("character images: "+characterImage[0]);
 
     // get count of length of data entries in JSON object
-//
+
     /*
     let neutralArray = [];
     for (var i = 0; i <= characterImage.length; i++) {

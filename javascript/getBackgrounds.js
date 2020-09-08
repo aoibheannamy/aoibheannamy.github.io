2c6a785ff2698e71709d5f9b66c0ce517273aa15
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-08 14:24:45 +0100

    this way should work

--- javascript/getBackgrounds.js
+++ javascript/getBackgrounds.js
@@ -26,35 +26,35 @@ function setBackgroundImage() {
      */
     switch (location) {
         case "BoardRoom":
-            bg = loadImage(locationBack[0]);
+            bg = loadImage(locationBack[0].BoardRoom);
             console.log("Board room loaded")
             break;
         case "Beach":
-            bg = loadImage(locationBack['Beach']);
+            bg = loadImage(locationBack[0].Beach);
             break;
         case "Forest":
-            bg = loadImage(locationBack['Forest']);
+            bg = loadImage(locationBack[0].Forest);
             break;
         case "Bedroom":
-            bg = loadImage(locationBack['Bedroom']);
+            bg = loadImage(locationBack[0].Bedroom);
             break;
         case "Club/Bar":
-            bg = loadImage(locationBack['Club/Bar']);
+            bg = loadImage(locationBack[0].Club/Bar);
             break;
         case "Office":
-            bg = loadImage(locationBack['Office']);
+            bg = loadImage(locationBack[0].Office);
             break;
         case "Prison":
-            bg = loadImage(locationBack['Prison']);
+            bg = loadImage(locationBack[0].Prison);
             break;
         case "Street":
-            bg = loadImage(locationBack['Street']);
+            bg = loadImage(locationBack[0].Street);
             break;
         case "Supermarket":
-            bg = loadImage(locationBack['Supermarket']);
+            bg = loadImage(locationBack[0].Supermarket);
             break;
         case "Warehouse":
-            bg = loadImage(locationBack['Warehouse']);
+            bg = loadImage(locationBack[0].Warehouse);
             break;
         default:
             break;

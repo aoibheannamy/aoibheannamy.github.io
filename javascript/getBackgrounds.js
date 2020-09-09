1f449c0a9ff7751775d18a31f1cb22ad0031e3bd
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-09 15:08:19 +0100

    pls work

--- javascript/getBackgrounds.js
+++ javascript/getBackgrounds.js
@@ -3,7 +3,7 @@
  * check which has been selected in the table,
  * load the corresponding image
  */
-function setBackgroundImage() {
+function setBackgroundImage(that) {
 
     // create variable to hold the parsed JSON file
     let locationBack = null;
@@ -25,44 +25,65 @@ function setBackgroundImage() {
     /**
      * Checks which background is selected and changes image dependent on the result
      */
- 
-    var dropdownBack = document.getElementById("ddlViewBy");
-    var backgrounds = dropdownBack.value;
-    console.log("Background selected:"+backgrounds);
 
-    switch (backgrounds) {
-        case "BoardRoom":
-            bg = loadImage(locationBack[0].BoardRoom);
-            console.log("Board room loaded");
-            break;
-        case "Beach":
-            bg = loadImage(locationBack[0].Beach);
-            break;
-        case "Forest":
-            bg = loadImage(locationBack[0].Forest);
-            break;
-        case "Bedroom":
-            bg = loadImage(locationBack[0].Bedroom);
-            break;
-        case "Club/Bar":
-            bg = loadImage(locationBack[0].Club / Bar);
-            break;
-        case "Office":
-            bg = loadImage(locationBack[0].Office);
-            break;
-        case "Prison":
-            bg = loadImage(locationBack[0].Prison);
-            break;
-        case "Street":
-            bg = loadImage(locationBack[0].Street);
-            break;
-        case "Supermarket":
-            bg = loadImage(locationBack[0].Supermarket);
-            break;
-        case "Warehouse":
-            bg = loadImage(locationBack[0].Warehouse);
-            break;
-        default:
-            break;
+    if (that.value == "BoardRoom") {
+        bg = loadImage(locationBack[0].BoardRoom);
+        console.log("Value:" + value);
+    } else if (that.value == "Beach") {
+        bg = loadImage(locationBack[0].Beach);
+    } else if (that.value == "Forest") {
+        bg = loadImage(locationBack[0].Forest);
+    } else if (that.value == "Bedroom") {
+        bg = loadImage(locationBack[0].Bedroom);
+    } else if (that.value == "Club/Value") {
+        bg = loadImage(locationBack[0].Club / Bar);
+    } else if (that.value == "Office") {
+        bg = loadImage(locationBack[0].Office);
+    } else if (that.value == "Prison") {
+        bg = loadImage(locationBack[0].Prison);
+    } else if (that.value == "Street") {
+        bg = loadImage(locationBack[0].Street);
+    } else if (that.value == "Supermarket") {
+        bg = loadImage(locationBack[0].Supermarket);
+    } else if (that.value == "Warehouse") {
+        bg = loadImage(locationBack[0].Warehouse);
     }
+
+
+    /*
+        switch (backgrounds) {
+            case "BoardRoom":
+                bg = loadImage(locationBack[0].BoardRoom);
+                console.log("Board room loaded");
+                break;
+            case "Beach":
+                bg = loadImage(locationBack[0].Beach);
+                break;
+            case "Forest":
+                bg = loadImage(locationBack[0].Forest);
+                break;
+            case "Bedroom":
+                bg = loadImage(locationBack[0].Bedroom);
+                break;
+            case "Club/Bar":
+                bg = loadImage(locationBack[0].Club / Bar);
+                break;
+            case "Office":
+                bg = loadImage(locationBack[0].Office);
+                break;
+            case "Prison":
+                bg = loadImage(locationBack[0].Prison);
+                break;
+            case "Street":
+                bg = loadImage(locationBack[0].Street);
+                break;
+            case "Supermarket":
+                bg = loadImage(locationBack[0].Supermarket);
+                break;
+            case "Warehouse":
+                bg = loadImage(locationBack[0].Warehouse);
+                break;
+            default:
+                break;
+        }*/
 }

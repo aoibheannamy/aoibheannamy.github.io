567035b4b9813424abf63b75118095983d7689cd
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-10 10:18:48 +0100

    update

--- javascript/getBackgrounds.js
+++ javascript/getBackgrounds.js
@@ -31,31 +31,31 @@ function setBackgroundImage(that) {
         console.log("Value:" + that.value + " bg: " + bg);
     } else if (that.value == "Beach") {
         id = locationBack.data[0].Beach;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Forest") {
         id = locationBack.data[0].Forest;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Bedroom") {
         id = locationBack.data[0].Bedroom;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Club/Bar") {
         id = locationBack.data[0].Club/Bar;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Office") {
         var id = locationBack.data[0].Office;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Prison") {
         var id = locationBack.data[0].Prison;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Street") {
         var id = locationBack.data[0].Street;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Supermarket") {
         var id = locationBack.data[0].Supermarket;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else if (that.value == "Warehouse") {
         var id = locationBack.data[0].Warehouse;
-        bg = loadImage("'"+id+"'");
+        bg = loadImage(id);
     } else{
         console.log("No background selected yet!");
     }

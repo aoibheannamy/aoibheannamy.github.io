f721bbcf6c36af1dc669dc3ecbf710b0ab7aba0f
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-10 10:39:54 +0100

    try this pls

--- javascript/getBackgrounds.js
+++ javascript/getBackgrounds.js
@@ -3,7 +3,7 @@
  * check which has been selected in the table,
  * load the corresponding image - dropdown
  */
-function setBackgroundImage(that) {
+function setBackgroundImage() {
 
     // create variable to hold the parsed JSON file
     let locationBack = null;
@@ -24,43 +24,44 @@ function setBackgroundImage(that) {
     /**
      * Checks which background is selected and changes image dependent on the result
      */
-
-    if (that.value == "BoardRoom") {
-        id = locationBack.data[0].BoardRoom;
-        console.log("ID: " + id);
-        bg = loadImage(id);
-        console.log("Value:" + that.value + " bg: " + bg);
-    } else if (that.value == "Beach") {
-        id = locationBack.data[0].Beach;
-        bg = loadImage(id);
-    } else if (that.value == "Forest") {
-        id = locationBack.data[0].Forest;
-        bg = loadImage(id);
-    } else if (that.value == "Bedroom") {
-        id = locationBack.data[0].Bedroom;
-        bg = loadImage(id);
-    } else if (that.value == "Club/Bar") {
-        id = locationBack.data[0].Club / Bar;
-        bg = loadImage(id);
-    } else if (that.value == "Office") {
-        var id = locationBack.data[0].Office;
-        bg = loadImage(id);
-    } else if (that.value == "Prison") {
-        var id = locationBack.data[0].Prison;
-        bg = loadImage(id);
-    } else if (that.value == "Street") {
-        var id = locationBack.data[0].Street;
-        bg = loadImage(id);
-    } else if (that.value == "Supermarket") {
-        var id = locationBack.data[0].Supermarket;
-        bg = loadImage(id);
-    } else if (that.value == "Warehouse") {
-        var id = locationBack.data[0].Warehouse;
-        bg = loadImage(id);
-    } else if (that.value == "") {
-        bg = loadImage('../Images/placeholderImage.jpg');
-    }
-
+    document.getElementById('my-select').addEventListener('change', function () {
+        console.log('You selected: ', this.value);
+        if (this.value == "BoardRoom") {
+            id = locationBack.data[0].BoardRoom;
+            console.log("ID: " + id);
+            bg = loadImage(id);
+            console.log("Value:" + this.value + " bg: " + bg);
+        } else if (this.value == "Beach") {
+            id = locationBack.data[0].Beach;
+            bg = loadImage(id);
+        } else if (this.value == "Forest") {
+            id = locationBack.data[0].Forest;
+            bg = loadImage(id);
+        } else if (this.value == "Bedroom") {
+            id = locationBack.data[0].Bedroom;
+            bg = loadImage(id);
+        } else if (this.value == "Club/Bar") {
+            id = locationBack.data[0].Club / Bar;
+            bg = loadImage(id);
+        } else if (this.value == "Office") {
+            var id = locationBack.data[0].Office;
+            bg = loadImage(id);
+        } else if (this.value == "Prison") {
+            var id = locationBack.data[0].Prison;
+            bg = loadImage(id);
+        } else if (this.value == "Street") {
+            var id = locationBack.data[0].Street;
+            bg = loadImage(id);
+        } else if (this.value == "Supermarket") {
+            var id = locationBack.data[0].Supermarket;
+            bg = loadImage(id);
+        } else if (this.value == "Warehouse") {
+            var id = locationBack.data[0].Warehouse;
+            bg = loadImage(id);
+        } else if (this.value == "") {
+            bg = loadImage('../Images/placeholderImage.jpg');
+        }
+    });
 
     /*
         switch (backgrounds) {

58da5dcfb88c9cb8ba5cb60c2d54bfa404e045f6
Aoibheann Maye <63245282+aoibheannamy@users.noreply.github.com>
2020-09-10 09:59:56 +0100

    add comments

--- javascript/getBackgrounds.js
+++ javascript/getBackgrounds.js
@@ -21,40 +21,43 @@ function setBackgroundImage() {
     }
     alert("stringified:" + locationBack);
 
+    /**
+     * Checks which background is selected and changes image dependent on the result
+     */
 
-        switch (backgrounds) {
-            case "BoardRoom":
-                bg = loadImage("'"+locationBack[0].BoardRoom+"'");
-                console.log("Board room loaded");
-                break;
-            case "Beach":
-                bg = loadImage("'"+locationBack.data[0].Beach+"'");
-                break;
-            case "Forest":
-                bg = loadImage("'"+locationBack.data[0].Forest+"'");
-                break;
-            case "Bedroom":
-                bg = loadImage("'"+locationBack.data[0].Bedroom+"'");
-                break;
-            case "Club/Bar":
-                bg = loadImage("'"+locationBack.data[0].Club / Bar+"'");
-                break;
-            case "Office":
-                bg = loadImage("'"+locationBack.data[0].Office+"'");
-                break;
-            case "Prison":
-                bg = loadImage("'"+locationBack.data[0].Prison+"'");
-                break;
-            case "Street":
-                bg = loadImage("'"+locationBack.data[0].Street+"'");
-                break;
-            case "Supermarket":
-                bg = loadImage("'"+locationBack.data[0].Supermarket+"'");
-                break;
-            case "Warehouse":
-                bg = loadImage("'"+locationBack.data[0].Warehouse+"'");
-                break;
-            default:
-                break;
-        }
+    switch (backgrounds) {
+        case "BoardRoom":
+            bg = loadImage("'" + locationBack[0].BoardRoom + "'");
+            console.log("Board room loaded");
+            break;
+        case "Beach":
+            bg = loadImage("'" + locationBack.data[0].Beach + "'");
+            break;
+        case "Forest":
+            bg = loadImage("'" + locationBack.data[0].Forest + "'");
+            break;
+        case "Bedroom":
+            bg = loadImage("'" + locationBack.data[0].Bedroom + "'");
+            break;
+        case "Club/Bar":
+            bg = loadImage("'" + locationBack.data[0].Club / Bar + "'");
+            break;
+        case "Office":
+            bg = loadImage("'" + locationBack.data[0].Office + "'");
+            break;
+        case "Prison":
+            bg = loadImage("'" + locationBack.data[0].Prison + "'");
+            break;
+        case "Street":
+            bg = loadImage("'" + locationBack.data[0].Street + "'");
+            break;
+        case "Supermarket":
+            bg = loadImage("'" + locationBack.data[0].Supermarket + "'");
+            break;
+        case "Warehouse":
+            bg = loadImage("'" + locationBack.data[0].Warehouse + "'");
+            break;
+        default:
+            break;
+    }
 }
